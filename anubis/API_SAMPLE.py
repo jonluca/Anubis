e5e7bb5f8c90d29ddb10c134bba563cafb62d554
JonLuca DeCaro <jdecaro@usc.edu>
2017-12-11 11:44:14 -0800

    CENSYS

--- anubis/API_SAMPLE.py
+++ anubis/API_SAMPLE.py
@@ -1,1 +1,5 @@
-SHODAN_KEY="<your api key here>"
+SHODAN_KEY = "<your api key here>"
+CENSYS_ID = "<your api id here>"
+CENSYS_SECRET = "<your api key here>"
+
+# Make sure to rename this file to API.py
