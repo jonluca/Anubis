39ae5359395045043207c02ef0150790611648e0
JonLuca DeCaro <jdecaro@usc.edu>
2017-12-12 16:29:17 -0800

    init passivetotal

--- anubis/API_SAMPLE.py
+++ anubis/API_SAMPLE.py
@@ -1,5 +1,7 @@
 SHODAN_KEY = "<your api key here>"
 CENSYS_ID = "<your api id here>"
 CENSYS_SECRET = "<your api key here>"
+PASSIVETOTAL_USERNAME = "<your passivetotal username here>"
+PASSIVETOTAL_KEY = "<your passivetotal key here>"
 
 # Make sure to rename this file to API.py
