5ce2c923fc10a270eb39bd3a01ad06ae511d62cc
JonLuca DeCaro <jdecaro@usc.edu>
2017-12-13 11:49:19 -0800

    convert to spaces

--- anubis/utils/ColorPrint.py
+++ anubis/utils/ColorPrint.py
@@ -1,27 +1,27 @@
 class ColorPrint:
-	RED = '\033[91m'
-	GREEN = '\033[92m'
-	YELLOW = '\033[93m'
-	LIGHT_PURPLE = '\033[94m'
-	PURPLE = '\033[95m'
-	END = '\033[0m'
+  RED = '\033[91m'
+  GREEN = '\033[92m'
+  YELLOW = '\033[93m'
+  LIGHT_PURPLE = '\033[94m'
+  PURPLE = '\033[95m'
+  END = '\033[0m'
 
-	@classmethod
-	def red(cls, s, **kwargs):
-		print(cls.RED + s + cls.END, **kwargs)
+  @classmethod
+  def red(cls, s, **kwargs):
+    print(cls.RED + s + cls.END, **kwargs)
 
-	@classmethod
-	def green(cls, s, **kwargs):
-		print(cls.GREEN + s + cls.END, **kwargs)
+  @classmethod
+  def green(cls, s, **kwargs):
+    print(cls.GREEN + s + cls.END, **kwargs)
 
-	@classmethod
-	def yellow(cls, s, **kwargs):
-		print(cls.YELLOW + s + cls.END, **kwargs)
+  @classmethod
+  def yellow(cls, s, **kwargs):
+    print(cls.YELLOW + s + cls.END, **kwargs)
 
-	@classmethod
-	def lightPurple(cls, s, **kwargs):
-		print(cls.LIGHT_PURPLE + s + cls.END, **kwargs)
+  @classmethod
+  def lightPurple(cls, s, **kwargs):
+    print(cls.LIGHT_PURPLE + s + cls.END, **kwargs)
 
-	@classmethod
-	def purple(cls, s, **kwargs):
-		print(cls.PURPLE + s + cls.END, **kwargs)
+  @classmethod
+  def purple(cls, s, **kwargs):
+    print(cls.PURPLE + s + cls.END, **kwargs)
--- setup.py
+++ setup.py
@@ -1,66 +1,46 @@
 """Packaging settings."""
 
-
 from codecs import open
 from os.path import abspath, dirname, join
 from subprocess import call
 
-from setuptools import Command, find_packages, setup
-
 from anubis import __version__
-
+from setuptools import Command, find_packages, setup
 
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
-    long_description = file.read()
+  long_description = file.read()
 
 
 class RunTests(Command):
-    """Run all tests."""
-    description = 'run tests'
-    user_options = []
+  """Run all tests."""
+  description = 'run tests'
+  user_options = []
 
-    def initialize_options(self):
-        pass
+  def initialize_options(self):
+    pass
 
-    def finalize_options(self):
-        pass
+  def finalize_options(self):
+    pass
 
-    def run(self):
-        """Run all tests!"""
-        errno = call(['py.test', '--cov=anubis', '--cov-report=term-missing'])
-        raise SystemExit(errno)
+  def run(self):
+    """Run all tests!"""
+    errno = call(['py.test', '--cov=anubis', '--cov-report=term-missing'])
+    raise SystemExit(errno)
 
 
-setup(
-    name = 'Anubis',
-    version = __version__,
-    description = 'Modern and efficient subdomain enumeration and information gathering',
-    long_description = long_description,
-    url = 'https://github.com/jonluca/anubis',
-    author = 'JonLuca DeCaro',
-    author_email = 'jdecaro@usc.edu',
-    license = 'UNLICENSE',
-    classifiers = [
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
-        'License :: Public Domain',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-	      'Programming Language :: Python :: 3.6',
-    ],
-    keywords = 'cli',
-    packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires = ['docopt'],
-    extras_require = {
-        'test': ['coverage', 'pytest', 'pytest-cov'],
-    },
-    entry_points = {
-        'console_scripts': [
-            'anubis=anubis.cli:main',
-        ],
-    },
-    cmdclass = {'test': RunTests},
-)
+setup(name='Anubis', version=__version__,
+  description='Modern and efficient subdomain enumeration and information gathering',
+  long_description=long_description, url='https://github.com/jonluca/anubis',
+  author='JonLuca DeCaro', author_email='jdecaro@usc.edu', license='UNLICENSE',
+  classifiers=['Intended Audience :: Developers', 'Topic :: Utilities',
+    'License :: Public Domain', 'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6', ], keywords='cli',
+  packages=find_packages(exclude=['docs', 'tests*']),
+  install_requires=['docopt'],
+  extras_require={'test': ['coverage', 'pytest', 'pytest-cov'], },
+  entry_points={'console_scripts': ['anubis=anubis.cli:main', ], },
+  cmdclass={'test': RunTests}, )
