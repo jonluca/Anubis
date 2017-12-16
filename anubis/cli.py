31b47d2115d8b562a25dec59b32789d3ee402762
JonLuca DeCaro <jdecaro@usc.edu>
2017-12-16 18:39:47 +0100

    recursive mostly functioning

--- anubis/cli.py
+++ anubis/cli.py
@@ -69,38 +69,42 @@ def seconds_to_str(t):
 
 
 def main():
-  if sys.version_info < (3, 0):
-    sys.stdout.write("Sorry, requires Python 3.x, not Python 2.x\n")
-    sys.exit(1)
+  try:
+    if sys.version_info < (3, 0):
+      sys.stdout.write("Sorry, requires Python 3.x, not Python 2.x\n")
+      sys.exit(1)
 
-  start_time = time.time()
+    start_time = time.time()
 
-  import anubis.commands
+    import anubis.commands
 
-  options = docopt(__doc__, version=VERSION)
+    options = docopt(__doc__, version=VERSION)
 
-  if options["--output"]:
-    sys.stdout = StdOutHook(options["FILENAME"])
+    if options["--output"]:
+      sys.stdout = StdOutHook(options["FILENAME"])
 
-  # Here we'll try to dynamically match the command the user is trying to run
-  # with a pre-defined command class we've already created.
-  if not options["--target"]:
-    print("Target required! Run with -h for usage instructions.")
-    return
+    # Here we'll try to dynamically match the command the user is trying to run
+    # with a pre-defined command class we've already created.
+    if not options["--target"]:
+      print("Target required! Run with -h for usage instructions.")
+      return
 
-  print("""
-        d8888                   888      d8b
-       d88888                   888      Y8P
-      d88P888                   888
-     d88P 888 88888b.  888  888 88888b.  888 .d8888b
-    d88P  888 888 "88b 888  888 888 "88b 888 88K
-   d88P   888 888  888 888  888 888  888 888 "Y8888b.
-  d8888888888 888  888 Y88b 888 888 d88P 888      X88
- d88P     888 888  888  "Y88888 88888P"  888  88888P'
-	""")
+    print("""
+          d8888                   888      d8b
+         d88888                   888      Y8P
+        d88P888                   888
+       d88P 888 88888b.  888  888 88888b.  888 .d8888b
+      d88P  888 888 "88b 888  888 888 "88b 888 88K
+     d88P   888 888  888 888  888 888  888 888 "Y8888b.
+    d8888888888 888  888 Y88b 888 888 d88P 888      X88
+   d88P     888 888  888  "Y88888 88888P"  888  88888P'
+    """)
 
-  command = anubis.commands.Target(options)
-  command.run()
-  print("Subdomain search took %s" % seconds_to_str(time.time() - start_time))
-  if options["--output"]:
-    sys.stdout.write_out()
+    command = anubis.commands.Target(options)
+    command.run()
+    print("Subdomain search took %s" % seconds_to_str(time.time() - start_time))
+    if options["--output"]:
+      sys.stdout.write_out()
+  except KeyboardInterrupt:
+    print("\nQuitting...")
+    sys.exit(0)
---
+++ anubis/utils/signal_handler.py
@@ -0,0 +1,31 @@
+import sys
+
+
+class SignalHandler:
+  """
+  The object that will handle signals and stop the worker threads.
+  """
+
+  #: The stop event that's shared by this handler and threads.
+  stopper = None
+
+  #: The pool of worker threads
+  workers = None
+
+  def __init__(self, stopper, workers):
+    self.stopper = stopper
+    self.workers = workers
+
+  def __call__(self, signum, frame):
+    """
+    This will be called by the python signal module
+
+    https://docs.python.org/3/library/signal.html#signal.signal
+    """
+    self.stopper.set()
+
+    for worker in self.workers:
+      worker.join()
+
+    print("Quitting...")
+    sys.exit(0)
