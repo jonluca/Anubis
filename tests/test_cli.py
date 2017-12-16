26f74c99af40e4288a96ba97cacd7da55205bbb5
JonLuca DeCaro <jdecaro@usc.edu>
2017-12-16 14:26:00 +0100

    init testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,13 +9,16 @@ from anubis import __version__ as VERSION
 class TestHelp(TestCase):
   def test_returns_usage_information(self):
     output = popen(['anubis', '-h'], stdout=PIPE).communicate()[0]
+    output = output.decode("utf-8")
     self.assertTrue('Usage:' in output)
 
     output = popen(['anubis', '--help'], stdout=PIPE).communicate()[0]
+    output = output.decode("utf-8")
     self.assertTrue('Usage:' in output)
 
 
 class TestVersion(TestCase):
   def test_returns_version_information(self):
     output = popen(['anubis', '--version'], stdout=PIPE).communicate()[0]
+    output = output.decode("utf-8")
     self.assertEqual(output.strip(), VERSION)
