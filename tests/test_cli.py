d318757afb03404a67529b9fb85997fa24a022b4
JonLuca DeCaro <jdecaro@usc.edu>
2017-12-16 15:15:32 +0100

    test for temp path and writeout

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,17 @@
 """Tests for our main anubis CLI module."""
 
+import os
+import shutil
+import tempfile
 from subprocess import PIPE, Popen as popen
 from unittest import TestCase
 
 from anubis import __version__ as VERSION
+from anubis.cli import StdOutHook
 
 
 class TestHelp(TestCase):
+
   def test_returns_usage_information(self):
     output = popen(['anubis', '-h'], stdout=PIPE).communicate()[0]
     output = output.decode("utf-8")
@@ -18,7 +23,26 @@ class TestHelp(TestCase):
 
 
 class TestVersion(TestCase):
+
   def test_returns_version_information(self):
     output = popen(['anubis', '--version'], stdout=PIPE).communicate()[0]
     output = output.decode("utf-8")
     self.assertEqual(output.strip(), VERSION)
+
+
+class TestOptions(TestCase):
+
+  def setUp(self):
+    # Create a temporary directory
+    self.test_dir = tempfile.mkdtemp()
+
+  def tearDown(self):
+    # Remove the directory after the test
+    shutil.rmtree(self.test_dir)
+
+  def test_stdouthook(self):
+    temp = StdOutHook(os.path.join(self.test_dir, 'test.txt'))
+    temp.write("test")
+    temp.write_out()
+    f = open(os.path.join(self.test_dir, 'test.txt'), 'r')
+    self.assertIn("test", f.readlines())
