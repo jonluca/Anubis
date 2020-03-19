55d99b2f21e5c56f0fc52398380d818b19ea0655
JonLuca DeCaro <jdecaro@usc.edu>
2020-03-19 16:15:26 -0700

    Updated URL due to loss of domain

--- anubis/scanners/anubis_db.py
+++ anubis/scanners/anubis_db.py
@@ -7,7 +7,7 @@ from anubis.utils.ColorPrint import ColorPrint
 
 def search_anubisdb(self, target):
   print("Searching Anubis-DB")
-  res = requests.get("https://jonlu.ca/anubis/subdomains/" + target)
+  res = requests.get("https://jldc.me/anubis/subdomains/" + target)
   if res.status_code == 200 and res.text:
     subdomains = loads(res.text)
     for subdomain in subdomains:
@@ -20,7 +20,7 @@ def send_to_anubisdb(self, target):
     print("Sending to AnubisDB")
     data = {'subdomains': dumps(self.domains)}
     # Sends found subdomains to Anubis (max 10,000/post)
-    res = requests.post("https://jonlu.ca/anubis/subdomains/" + target[0],
+    res = requests.post("https://jldc.me/anubis/subdomains/" + target[0],
                         data=data)
     if res.status_code != 200:
       ColorPrint.red("Error sending results to AnubisDB - Status Code: " + str(
